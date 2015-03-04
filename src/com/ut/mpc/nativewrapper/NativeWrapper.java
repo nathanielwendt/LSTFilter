93917a833b4cb285bcbc460eedb65974ca955023
Nate Wendt <nwendt22@gmail.com>
2015-03-04 01:42:58 -0600

    implemented native wrapper and demo runner to run rtree operations outside of android

---
+++ src/com/ut/mpc/nativewrapper/NativeWrapper.java
@@ -0,0 +1,7 @@
+package com.ut.mpc.nativewrapper;
+
+/**
+ * Created by nathanielwendt on 2/19/15.
+ */
+public class NativeWrapper {
+}
---
+++ src/com/ut/mpc/workload/CabsWrapper.java
@@ -0,0 +1,43 @@
+package com.ut.mpc.workload;
+
+import com.ut.mpc.utils.STPoint;
+import com.ut.mpc.utils.STStorage;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+
+public class CabsWrapper {
+
+    //@pre: requires at least one point in file, otherwise seg. fault
+    //@pre: requires first entry to be most recent and last entry to be least recent
+    public static void fillPointsFromFile(STStorage[] trees, String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new FileReader("../Crawdad/cabspottingdata/new_abboip.txt"));
+        //BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/../Crawdad/mobilitydata/KAIST/" + args[0]));
+        String line;
+        line = br.readLine();
+        String[] split = new String[3];
+        split = line.split("\t");
+        Long timestamp = Double.valueOf(split[0]).longValue();
+//        Init.REFERENCE_TIMESTAMP = timestamp; //should be 0.00 for mobility data
+ //       Init.CURRENT_TIMESTAMP = timestamp; //continually update current to simulate real-time insertion of points
+        STPoint point = new STPoint(Float.valueOf(split[0]),Float.valueOf(split[1]),Float.valueOf(split[2]));
+        insertPoint(trees, point);
+
+        while ((line = br.readLine()) != null) {
+            split = line.split("\t");
+//s          Init.CURRENT_TIMESTAMP = timestamp; //continually update current to simulate real-time insertion of points
+            point = new STPoint(Float.valueOf(split[0]),Float.valueOf(split[1]),Float.valueOf(split[2]));
+            insertPoint(trees, point);
+        }
+        br.close();
+    }
+
+    public static void insertPoint(STStorage[] trees, STPoint temp){
+        for(int i = 0; i < trees.length; i++){
+            trees[i].insert(temp);
+        }
+    }
+
+}
